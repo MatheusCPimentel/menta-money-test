57e537ae15ae66b4200002c53558ef7098f5ff92
Matheus Pimentel <matheus08.pimentel@gmail.com>
2023-06-17 21:58:42 -0300

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,23 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    // extend: {
+    colors: {
+      white: '#FFFFFF',
+      'gray-1': '#121214',
+      'gray-2': '#202024',
+      'gray-3': '#29292E',
+      'gray-4': '#323238',
+      'gray-5': '#7C7C8A',
+      'gray-6': '#C4C4CC',
+      'gray-7': '#E1E1E6',
+      'green-dark': '#015F43',
+      green: '#00875F',
+      'green-light': '#00B37E',
+      'red-dark': '#AA2834',
+      red: '#F75A68',
     },
+    // },
   },
   plugins: [],
 }
