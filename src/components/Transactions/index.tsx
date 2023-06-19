75bc8fe956fa034702b03b32f934a32e6e806e48
Matheus Pimentel <matheus08.pimentel@gmail.com>
2023-06-18 23:53:40 -0300

    Created search method with filter

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -9,6 +9,11 @@ import { Input } from '../Input'
 
 export const Transactions = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [searchInputValue, setSearchInputValue] = useState('')
+
+  const [transactionsToShow, setTransactionsToShow] = useState<Transaction[]>(
+    [],
+  )
 
   useEffect(() => {
     async function getTransactions() {
@@ -18,6 +23,7 @@ export const Transactions = () => {
         )
 
         setTransactions(transactions)
+        setTransactionsToShow(transactions)
       } catch (error) {
         console.log(`Houve um erro ao tentar carregar as transações: ${error}`)
       }
@@ -26,25 +32,43 @@ export const Transactions = () => {
     getTransactions()
   }, [])
 
+  const handleSearchTransaction = () => {
+    const sanitizedSearchInputValue = searchInputValue?.toLowerCase().trim()
+
+    const filteredTransactions = transactions.filter((transaction) =>
+      transaction.title.toLowerCase().includes(sanitizedSearchInputValue),
+    )
+
+    setTransactionsToShow(filteredTransactions)
+  }
+
   return (
     <>
       <div className="mt-6">
         <div className="mb-3 flex items-center justify-between">
           <span className="text-lg">Transações</span>
-          <span className="text-gray-5">4 itens</span>
+          <span className="text-gray-5">{transactions?.length || 0} itens</span>
         </div>
 
         <div className="flex items-center gap-2">
-          <Input placeholder="Busque uma transação" />
+          <Input
+            placeholder="Busque uma transação"
+            value={searchInputValue}
+            onChange={(e) => setSearchInputValue(e?.currentTarget?.value)}
+            onKeyDown={(e) => e.key === 'Enter' && handleSearchTransaction()}
+          />
 
-          <div className="cursor-pointer rounded-md border border-green-light p-4 text-green-light">
+          <div
+            className="cursor-pointer rounded-md border border-green-light p-4 text-green-light"
+            onClick={() => handleSearchTransaction()}
+          >
             <Search size={22} />
           </div>
         </div>
       </div>
 
       <main className="mt-3 flex flex-col gap-3">
-        {transactions.map((transaction) => (
+        {transactionsToShow.map((transaction) => (
           <TransactionCard
             key={transaction.id}
             amount={transaction.amount}
